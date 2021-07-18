a621a817719d4b697cb9da4a6b22ff01625bdaf7
Anıl Yekta <37214615+yek-ta@users.noreply.github.com>
2021-07-18 22:37:08 +0300

    Add files via upload

---
+++ YazlikTelegramBotUygulamasi/YazlikTelegramBotUygulamasi/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace YazlikTelegramBotUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.Name = "listBox1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 21600000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::YazlikTelegramBotUygulamasi.Properties.Resources.tedaswall;
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ YazlikTelegramBotUygulamasi/YazlikTelegramBotUygulamasi/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telegram.Bot;
+
+namespace YazlikTelegramBotUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        static TelegramBotClient Bot = new TelegramBotClient("65asdasd6021:AadasdasdpD9MoNzQRY_fgd");
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Add("(" + DateTime.Now.ToString("h:mm") + ") Program Açıldı..");
+            label1.Text = "Düzenli olarak " + ((timer1.Interval/1000)/60)/60 + " saatte bir mesaj gönderilecek..";
+            Bot.StartReceiving();
+            Bot.OnMessage += Bot_OnMessage;
+
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+
+            Bot.SendTextMessageAsync(187921443, "Program Açıldı");
+        }
+
+        private void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if(e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                Bot.SendTextMessageAsync(e.Message.Chat.Id, "Yaşıyorum!! "+ e.Message.Chat.Username);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bot.SendTextMessageAsync(187921443, "Sistem Çalışmaktadır..");
+            listBox1.Items.Add("(" + DateTime.Now.ToString("h:mm") + ") Mesaj Gönderildi.");
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
