cd61483a8d2afb18668e68457679427117b7f41a
Anıl Yekta <37214615+yek-ta@users.noreply.github.com>
2021-08-08 13:44:00 +0300

    Add files via upload

---
+++ AmxxCompilerWithTelegram/AmxxCompilerWithTelegram/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace AmxxCompilerWithTelegram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(718, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Programı Kapat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AmxxCompilerWithTelegram.Properties.Resources.amxxcompilerarayuz;
+            this.ClientSize = new System.Drawing.Size(1167, 128);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Telegram AmxxCompiler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AmxxCompilerWithTelegram/AmxxCompilerWithTelegram/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using Telegram.Bot;
+using System.Diagnostics;
+
+namespace AmxxCompilerWithTelegram
+{
+    
+    public partial class Form1 : Form
+    {
+        bool derleyebilir;
+        string kuruludizin = Application.StartupPath;
+        static TelegramBotClient Bot = new TelegramBotClient("18asdasdasdaqqweqwjDOihtHUtA");
+        public Form1()
+        {
+            derleyebilir = true;
+            InitializeComponent();
+            Bot.StartReceiving();
+            Bot.OnMessage += Bot_OnMessage;
+            
+
+            Bot.SendTextMessageAsync(187921443, "Program Açıldı");
+            //Directory.CreateDirectory("D:\\telegramcompiler");
+            label1.Text = kuruludizin;
+
+        }
+        private async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                //Bot.SendTextMessageAsync(e.Message.Chat.Id, "Yaşıyorum!! " + e.Message.Chat.Username);
+                
+            }
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+            {
+                if (derleyebilir)
+                {
+                    derleyebilir = false;
+                    //Bot.SendTextMessageAsync(e.Message.Chat.Id, "dosya geldi!! " + e.Message.Document);
+
+                    Telegram.Bot.Types.File file = await Bot.GetFileAsync(e.Message.Document.FileId);
+
+                    string fileName = file.FileId + "." + file.FilePath.Split('.').Last();
+
+                    using (FileStream saveFileStream = File.Open(fileName, FileMode.Create))
+                    {
+                        await Bot.DownloadFileAsync(file.FilePath, saveFileStream);
+                    }
+                    await Bot.SendTextMessageAsync(e.Message.Chat.Id, "Dosya Sunucuya Ulaştırıldı..");
+                    
+                    //File.Copy( + fileName, @"D:\telegramcompiler");
+                    Process.Start(kuruludizin + @"\compile.exe");
+
+                    System.Threading.Thread.Sleep(2000);
+                    System.IO.File.Move(fileName, @"silinmeli\"+fileName);
+                    string yenisim= fileName.Replace("sma", "amxx");
+                    yenisim = @"compiled\" + yenisim;
+                    if (!File.Exists(yenisim))
+                    {
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, "Dosya ile ilgili sıkıntı var, derleyemedim " + e.Message.Chat.Username);
+                    }
+                    else
+                    {
+                        using (var sendFileStream = File.Open(yenisim, FileMode.Open))
+                        {
+                            await Bot.SendDocumentAsync(e.Message.Chat.Id, new Telegram.Bot.Types.InputFiles.InputOnlineFile(sendFileStream, yenisim));
+                        }
+                    }
+                }
+                else
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Derleyicimin kapatılması için bekleyiniz " + e.Message.Chat.Username);
+                }
+                
+
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            derleyebilir = true;
+
+
+            string processName = "compile"; // Kapatmak İstediğimiz Program
+            Process[] processes = Process.GetProcesses();// Tüm Çalışan Programlar
+            foreach (Process process in processes)
+            {
+                if (process.ProcessName == processName)
+                {
+                    process.Kill();
+                }
+
+            }
+            label1.Text = "Derleyici Kapatıldı";
+        }
+    }
+}
